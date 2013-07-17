6d258731e97b60c4485701253c9c4b09f1cbb767
Mauricio Rivera <maurlamf@Mauricios-MacBook-Air.local>
2013-07-17 16:14:52 -0700

    bootstrap

--- web.js
+++ web.js
@@ -4,10 +4,10 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     console.log('index')
-var readStream= = fs.createReadStream('index.html').pipe(response)
+var readStream = fs.createReadStream('index.html').pipe(response)
 
 });
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
